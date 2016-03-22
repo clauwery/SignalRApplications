b1f85026f24d0e3d604904122cca3188a44b03ab
christophe.lauwerys@gmail.com <christophe.lauwerys@gmail.com>
2016-03-22 08:12:16 +0100

    - Added jTable to SignalRChat Server projects - Further refined XIL API handling

--- WpfApplicationClient/MainWindow.xaml.cs
+++ WpfApplicationClient/MainWindow.xaml.cs
@@ -30,15 +30,14 @@ namespace XilApiTools
     class Connection_Basics
     {
         static Type serverType = Type.GetTypeFromProgID("DSPlatformManagementAPI2");
-        IPmPlatformManagement PlatformManagement = Activator.CreateInstance(serverType) as IPmPlatformManagement;
-        public IPmSeekedPlatforms Platforms;
+        public IPmPlatformManagement PlatformManagement = Activator.CreateInstance(serverType) as IPmPlatformManagement;
         public string RegisterPlatform(string platformName)
         {
             string Message = "";
             try
             {
                 PlatformType platformType = (PlatformType)Enum.Parse(typeof(PlatformType), platformName);
-                IPmMABXRegisterInfo RegisterInfo = (IPmMABXRegisterInfo)PlatformManagement.CreatePlatformRegistrationInfo(platformType);
+                var RegisterInfo = PlatformManagement.CreatePlatformRegistrationInfo(platformType);
                 PlatformManagement.RegisterPlatform(RegisterInfo);
             }
             catch(Exception ex)
@@ -60,33 +59,19 @@ namespace XilApiTools
             }
             return Message;
         }
-        public void GetPlatforms()
-        {
-            Platforms = (IPmSeekedPlatforms)PlatformManagement.Platforms;
-        }
-
-/*        public string[] GetPlatformTypes()
-        {
-            foreach(PlatformType foo in PlatformType.GetValues(typeof(PlatformType)))
-            {
-                platformType[] = foo.ToString();
-            }
-            
-        }
-        */
-        
     }
 
     class Port_Basics
     {
-        private string MAPortConfigFile = @"..\Models\MAPortConfig.xml";
+        private string MAPortConfigFile = @"MAPortConfig.xml";
         private string vendorName = "dSPACE GmbH";
         private string productName = "XIL API";
         private string productVersion = "2015-B";
-        private IList<string> VariableNames;
+        private IEnumerator<string> variableNames;
         public IMAPort MAPort;
-        public void Initialise()
+        public string Initialise()
         {
+            string message = "";
             try
             {
                 ITestbenchFactory TBFactory = new TestbenchFactory();
@@ -94,32 +79,40 @@ namespace XilApiTools
                 MAPort = TB.MAPortFactory.CreateMAPort("MAPort");
                 IMAPortConfig maPortConfig = MAPort.LoadConfiguration(MAPortConfigFile);
                 MAPort.Configure(maPortConfig, false);
+                variableNames = MAPort.VariableNames.GetEnumerator();
             }
+/*
             catch (TestbenchPortException ex)
             {
-                Console.WriteLine("A XIL API TestbenchPortException occurred:");
                 Console.WriteLine("Error description: {0}", ex.CodeDescription);
                 Console.WriteLine("Detailed error description: {0}", ex.VendorCodeDescription);
-                Console.ReadLine();
             }
+*/
             catch (System.Exception ex)
             {
-                Console.WriteLine("A system exception occurred:");
-                Console.WriteLine("Error description: {0}", ex.Message);
+                message = ex.Message;
+                MAPort.Dispose();
             }
-            finally
-            {
-                // Dispose any instances of MAPort, Capture, and EESPort
-            }
+            /*
+                        finally
+                        {
+                            // Dispose any instances of MAPort, Capture, and EESPort
+                        }
+              */
+            return message;
         }
         public double Read(string variableName)
         {
-            if (MAPort!=null && MAPort.State==MAPortState.eSIMULATION_RUNNING && MAPort.VariableNames.Contains(variableName) && MAPort.IsReadable(variableName))
+            try
             {
-                IFloatValue value = (IFloatValue)MAPort.Read(variableName);
-                return value.Value;
+                if (MAPort != null && MAPort.State == MAPortState.eSIMULATION_RUNNING && MAPort.VariableNames.Contains(variableName) && MAPort.IsReadable(variableName))
+                {
+                    IFloatValue value = (IFloatValue)MAPort.Read(variableName);
+                    return value.Value;
+                }
+                return -1;
             }
-            else
+            catch (Exception ex)
             {
                 return -1;
             }
@@ -147,9 +140,7 @@ namespace WpfApplicationClient
             connection.StateChanged += new Action<StateChange>(OnMyEvent);
             // Populate platform types
             // WHERE SHOULD I DO THIS?
-            foreach (PlatformType s in Enum.GetValues(typeof(PlatformType)))
-                comboBox.Items.Add(s.ToString());
-            comboBox.SelectedIndex = 0;
+            update_platform_listBox();
         }
         private void OnMyEvent(StateChange obj)
         {
@@ -204,28 +195,47 @@ namespace WpfApplicationClient
         }
         private void register_xil_button_Click(object sender, RoutedEventArgs e)
         {
-            string platformName = comboBox.SelectedItem.ToString();
+            // string platformName = comboBox.SelectedItem.ToString();
+            string platformName = "MABX";
             status_message_text.Text = XilConnection.RegisterPlatform(platformName);
-            update_platform_list();
-            PortConnection.Initialise();
+            update_platform_listBox();
         }
         private void clear_xil_button_Click(object sender, RoutedEventArgs e)
         {
             status_message_text.Text = XilConnection.Clear();
-            update_platform_list();
+            update_platform_listBox();
         }
-        private void update_platform_list()
+        private void update_platform_listBox()
         {
-            if (XilConnection.Platforms != null)
+            platform_listBox.Items.Clear();
+            // Auto get type of platform !!! not always MABX
+            foreach (IPmMABXPlatform item in (IPmSeekedPlatforms)XilConnection.PlatformManagement.Platforms)
             {
-                platforms.Text = XilConnection.Platforms.Count.ToString();
+                platform_listBox.Items.Add(item.DisplayName);
             }
+            platform_listBox.SelectedIndex = 0;
+        }
+        private void update_variable_listBox()
+        {
+            variable_listBox.Items.Clear();
+            foreach (string item in PortConnection.MAPort.VariableNames)
+                {
+                    variable_listBox.Items.Add(item);
+                }
+            variable_listBox.SelectedIndex = 0;
         }
         private void read_xil_variable_button_Click(object sender, RoutedEventArgs e)
         {
-            string variableName = "Platform()://currentTime";
+            // string variableName = "ds1401()://currentTime";
+            string variableName = variable_listBox.SelectedItem.ToString();
             read_xil_variable_text.Text = PortConnection.Read(variableName).ToString();
-            
+        }
+        private void connect_maport_button_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                status_message_text.Text = PortConnection.Initialise();
+                update_variable_listBox();
+            }
         }
     }
 }
